647804b0e2d9bd3cf2841e62b206159e90c39308
ChrozaGaming <hilmyraihankindy@gmail.com>
2024-03-23 10:58:58 +0700

    Add Footer component to DefaultLayout, router to main, and update ProductDetailPage styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
 import ProductDetailPage from "./pages/ProductDetailPage";
@@ -12,22 +12,22 @@ import CartPage from "./pages/CartPage";
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <HomePage />,
+        element: <HomePage/>,
     },
     {
         path: "/product/:id",
-        element: <ProductDetailPage />,
+        element: <ProductDetailPage/>,
     },
     {
         path: "/cart",
-        element: <CartPage />,
+        element: <CartPage/>,
     },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <ProductContextProvider>
-            <RouterProvider router={router} />
+            <RouterProvider router={router}/>
         </ProductContextProvider>
     </React.StrictMode>
 );
